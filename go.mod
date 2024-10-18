09d1320d10978fe5784cb686411e0b4876ad47fe
Terry Toolin <toolinfamily@msn.com>
2024-10-18 10:02:41 -0700

    third commit

--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module github.com/ttoolin/puppy
 
 go 1.23.2
+
+require github.com/ttoolin/dog v0.0.0-20241018164033-3cf9a79c15ff // indirect
--- puppy.go
+++ puppy.go
@@ -1,9 +1,21 @@
 package puppy
 
+import (
+	"github.com/ttoolin/dog"
+)
+
 func Bark() string {
 	return "woof!"
 }
 
 func Barks() string {
 	return "woof woof woof"
+}
+
+func BigBark() string {
+	return dog.WhenGrowsUp(Bark())
+}
+
+func BigBarks() string {
+	return dog.WhenGrowsUp(Barks())
 }
